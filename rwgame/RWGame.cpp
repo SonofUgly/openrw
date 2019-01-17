538324899c9de97176c5e6a79507964f332d8bb0
SonofUgly <son_of_ugly@yahoo.com>
2019-01-16 17:36:26 -0800

    Update RWGame.cpp

--- rwgame/RWGame.cpp
+++ rwgame/RWGame.cpp
@@ -33,7 +33,7 @@ static constexpr std::array<
                     {GameRenderer::ZoneCylinderB, "zonecylb.dff", "particle"},
                     {GameRenderer::Arrow, "arrow.dff", ""}}};
 
-constexpr float kMaxPhysicsSubSteps = 4;
+constexpr float kMaxPhysicsSubSteps = 0;
 }  // namespace
 
 #define MOUSE_SENSITIVITY_SCALE 2.5f
